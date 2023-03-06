da7dc71cc3ece9b0f270c982fe825f0098100add
CheshireCaat <likangt2012@gmail.com>
2023-03-06 08:40:41 +0300

    docs: add an example of interaction with cookies
    Closes: #4

---
+++ examples/cookies.js
@@ -0,0 +1,23 @@
+require('chromedriver');
+require('dotenv').config();
+
+const { plugin } = require('..');
+const { until, By } = require('selenium-webdriver');
+
+(async () => {
+  const driver = await plugin.launch();
+
+  await driver.get('https://google.com');
+  await driver.wait(until.elementLocated(By.css('form[action]')));
+
+  // By default selenium saves cookies only for the current domain.
+  // We can use `CDP` commands to get all browser cookies.
+  const { cookies } = await driver.sendAndGetDevToolsCommand('Storage.getCookies');
+  console.log('Cookies:', cookies);
+
+  // Other methods of interacting with cookies in selenium also work within the current domain.
+  // So, for example, you can also use `CDP` to import/clear all browser cookies:
+  await driver.sendDevToolsCommand('Storage.clearCookies');
+
+  await driver.quit();
+})();
